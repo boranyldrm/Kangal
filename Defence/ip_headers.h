6c8855ffb9195734028fc803186d5dfd4054fdf1
boranyldrm <boranyldrm21@gmail.com>
2017-07-14 14:18:10 +0300

    header fix

--- Defence/ip_headers.h
+++ Defence/ip_headers.h
@@ -1,3 +1,10 @@
+//
+// Created by Boran Yildirim on 13/07/2017.
+//
+
+#ifndef VLAN_HOPPING_IP_HEADERS_H
+#define VLAN_HOPPING_IP_HEADERS_H
+
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/types.h>
@@ -25,14 +32,14 @@ struct ip_header {
     u_short ip_len;                 /* total length */
     u_short ip_id;                  /* identification */
     u_short ip_off;                 /* fragment offset field */
-    #define IP_RF 0x8000            /* reserved fragment flag */
-    #define IP_DF 0x4000            /* dont fragment flag */
-    #define IP_MF 0x2000            /* more fragments flag */
-    #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+#define IP_RF 0x8000            /* reserved fragment flag */
+#define IP_DF 0x4000            /* dont fragment flag */
+#define IP_MF 0x2000            /* more fragments flag */
+#define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
     u_char  ip_ttl;                 /* time to live */
     u_char  ip_p;                   /* protocol */
     u_short ip_sum;                 /* checksum */
-    struct  in_addr ip_src,ip_dst;  /* source and dest address */
+    struct  in_addr ip_src, ip_dst;  /* source and dest address */
 };
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
@@ -47,20 +54,27 @@ struct tcp_header {
     tcp_seq th_seq;                 /* sequence number */
     tcp_seq th_ack;                 /* acknowledgement number */
     u_char  th_offx2;               /* data offset, rsvd */
-	#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
     u_char  th_flags;
-    #define TH_FIN  0x01
-    #define TH_SYN  0x02
-    #define TH_RST  0x04
-   	#define TH_PUSH 0x08
-    #define TH_ACK  0x10
-    #define TH_URG  0x20
-    #define TH_ECE  0x40
-    #define TH_CWR  0x80
-    #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
     u_short th_win;                 /* window */
     u_short th_sum;                 /* checksum */
     u_short th_urp;                 /* urgent pointer */
+};
+
+
+struct vlan_tag_header {
+    u_short	tpid;	/*Tag protocol identifier*/
+    u_char pcp_dei;	/* priority = 0 default, drop eligible = 0*/
+    u_short vid;	/*VLAN identifier*/
 };
 
 
@@ -68,27 +82,20 @@ struct tcp_header {
 struct vlan_ethernet_header {
     u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
     u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
-	struct 	vlan_tag_header * customer;			
-	struct 	vlan_tag_header * service;
+    struct 	vlan_tag_header customer;
+    struct 	vlan_tag_header service;
     u_short ether_type;                     /* IP? ARP? RARP? etc */
 };
 
 
 /* ICMP header */
 struct icmp_header {
- 	u_char  icmph_type;
- 	u_char  icmph_code;
- 	u_short icmph_chksum;
- 	/* The following data structures are ICMP type specific */
- 	u_short icmph_ident;
- 	u_short icmph_seqnum;
+    u_char  icmph_type;
+    u_char  icmph_code;
+    u_short icmph_chksum;
+    /* The following data structures are ICMP type specific */
+    u_short icmph_ident;
+    u_short icmph_seqnum;
 }; /* total ICMP header length: 8 bytes (= 64 bits) */
 
-
-struct vlan_tag_header {
-	u_short	tpid;	/*Tag protocol identifier*/
-	u_char pcp_dei;	/* priority = 0 default, drop eligible = 0*/
-	u_short vid;	/*VLAN identifier*/
-};
-
-
+#endif //VLAN_HOPPING_IP_HEADERS_H
